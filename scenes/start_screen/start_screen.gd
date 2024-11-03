bc23fe8edc80ef6e79b84174c8d4564ed1bc0e9a
Dion Tryban <github@diontryban.com>
2024-11-03 10:28:37 -0500

    Animate orca on start screen

--- scenes/start_screen/start_screen.gd
+++ scenes/start_screen/start_screen.gd
@@ -1,6 +1,11 @@
 extends Node3D
 
 @onready var main_scene: PackedScene = load("res://scenes/main/main.tscn")
+@onready var orca_anim_player: AnimationPlayer = $OrkaBlendModel/AnimationPlayer
+
+
+func _process(delta):
+	orca_anim_player.play("Idle")
 
 
 func _on_button_pressed():
