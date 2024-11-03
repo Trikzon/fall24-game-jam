eb2d90d92db14859bf8f24fd2f843cb8bec8a59e
Time Alchemist <96148194+TimeAlchemist@users.noreply.github.com>
2024-11-02 20:14:10 -0400

    Adds echo locate attack

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -7,6 +7,8 @@ const MAX_SPEED = 5.0
 @export var ground_raycast_back: RayCast3D
 @export var ground_raycast_front: RayCast3D
 
+@onready var blast_scene: PackedScene = load("res://entities/Attacks/blast.tscn")
+
 
 func _physics_process(delta):
 	#Add the gravity.	
@@ -14,10 +16,36 @@ func _physics_process(delta):
 		velocity -= camera_target.transform.basis.z * ACCELERATION * delta
 		rotation.y = lerp_angle(rotation.y, camera_target.rotation.y, 10 * delta)
 		rotation.x = lerp_angle(rotation.x, camera_target.rotation.x, 10 * delta)
-		$OrkaBlendModel.get_child(1).play("ArmatureAction")
+		if not $OrkaBlendModel.get_child(1).is_playing():
+			$OrkaBlendModel.get_child(1).play("Swim")
 	else:
+		if not $OrkaBlendModel.get_child(1).is_playing():
+			$OrkaBlendModel.get_child(1).play("Idle")
 		velocity = lerp(velocity, Vector3.ZERO, 2 * delta)
-	
+	#attack
+	if Input.is_action_just_pressed("Attack"):
+		var blast: Blast = blast_scene.instantiate()
+		add_sibling(blast)
+		blast.position = position
+		blast.transform.basis.z += 3.75 * Vector3(1,1,1)
+		blast.rotation = rotation
+		
+		await get_tree().create_timer(0.1).timeout
+		
+		blast = blast_scene.instantiate()
+		add_sibling(blast)
+		blast.position = position
+		blast.transform.basis.z += 3.75 * Vector3(1,1,1)
+		blast.rotation = rotation
+		
+		await get_tree().create_timer(0.1).timeout
+		
+		blast = blast_scene.instantiate()
+		add_sibling(blast)
+		blast.position = position
+		blast.transform.basis.z += 3.75 * Vector3(1,1,1)
+		blast.rotation = rotation
+		
 	#if not ground_raycast_front.is_colliding() and not ground_raycast_front.is_colliding():
 		#velocity += get_gravity() * delta
 	
