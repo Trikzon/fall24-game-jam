cbd271c9584a586f18a4c5a9c060f5704df117e8
Dion Tryban <github@diontryban.com>
2024-11-07 14:40:50 -0500

    Finish adding puffer ability

--- entities/puffer.gd
+++ entities/puffer.gd
@@ -25,21 +25,15 @@ func _physics_process(delta):
 func blowup():
 	moving=false
 	animation_player.play("ArmatureAction_001")
-	await get_tree().create_timer(1.6).timeout
+	
+	for body in $Area3D2.get_overlapping_bodies():
+		if body is Enemy:
+			for i in range(5):
+				body.take_damage()
+	
+	await get_tree().create_timer(2).timeout
 	queue_free()
 
 func _on_area_3d_body_entered(body):
 	if((not animation_player.is_playing())and((body is Enemy) or (body is Terrain3D))):
 		blowup()
-	#if body is Enemy:
-		#body.take_damage()
-	#elif body is Terrain3D:
-		#await get_tree().create_timer(0.25).timeout
-		#queue_free() # Replace with function body.
-
-func _on_area_3d_2_body_entered(body):
-	if(moving==false and body is Enemy):
-		for i in range(20):
-			body.take_damage()
-		print(body)
-		print(body.health)
